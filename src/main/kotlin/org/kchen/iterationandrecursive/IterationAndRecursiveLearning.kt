d7d4e2afffbfa77e5c39b81f3fe7b70c1c361a7d
kissinger <kchen0723@gmail.com>
2025-04-02 22:12:26 -0700

    add sample class

---
+++ src/main/kotlin/org/kchen/iterationandrecursive/IterationAndRecursiveLearning.kt
@@ -0,0 +1,9 @@
+package org.kchen.iterationandrecursive
+
+public class IterationAndRecursiveLearning {
+    fun iterationArray(arr: List<Int>){
+        for(item in arr){
+            println(item);
+        }
+    }
+}
---
+++ src/test/kotlin/org/kchen/iterationandrecursive/IterationAndRecursiveLearningTest.kt
@@ -0,0 +1,12 @@
+package org.kchen.iterationandrecursive
+
+import org.junit.jupiter.api.Test
+
+class IterationAndRecursiveLearningTest {
+    //private val iterationInstance = org.kchen.iterationandrecursive.IterationAndRecursiveLearning();
+    @Test
+    fun iterationArrayTest(){
+        var numbers = mutableListOf(4, 7, 9, 11);
+        //iterationInstance.iterationArray(numbers);
+    }
+}
